8a09c3ae6b4b351319abaf6d60007aed0358d791
Neal Chang <prince811009fy@gmail.com>
2026-07-12 17:28:32 +0800

    Connect Flutter forecast page to Python output

--- lib/services/forecast_service.dart
+++ lib/services/forecast_service.dart
@@ -1,1 +1,23 @@
-class ForecastService {}
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+import '../models/forecast_result.dart';
+
+class ForecastService {
+  Future<ForecastResult> loadForecastResult() async {
+    final jsonString = await rootBundle.loadString(
+      'assets/forecast/forecast_result.json',
+    );
+
+    final jsonData = jsonDecode(jsonString);
+
+    if (jsonData is! Map<String, dynamic>) {
+      throw const FormatException(
+        'Invalid forecast JSON format.',
+      );
+    }
+
+    return ForecastResult.fromJson(jsonData);
+  }
+}
