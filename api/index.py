3f315af40a01c5f5a250c98caaad6c6317100cc7
Neal Chang <prince811009fy@gmail.com>
2026-07-12 23:34:04 +0800

    Prepare FastAPI for Vercel deployment

---
+++ api/index.py
@@ -0,0 +1,1 @@
+from python.api import app
--- python/api.py
+++ python/api.py
@@ -16,7 +16,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1):\d+$",
+    allow_origin_regex=(
+        r"^https://.*\.vercel\.app$"
+        r"|^http://(localhost|127\.0\.0\.1):\d+$"
+    ),
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
